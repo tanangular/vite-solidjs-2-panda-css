c6dd9d58a1d59813d2cb10f34712ef729b5c0a21
tanangular <tanangular@gmail.com>
2026-04-18 16:36:44 +0700

    ปรับปรุงตัวอย่างการใช้งาน createSignal และ createMemo เพื่อสาธิตการควบคุมระบบ Reactive
    เพิ่มตัวอย่างการใช้ pureWrite ใน createSignal และการกำหนด equals เป็น false ใน createMemo พร้อมเปรียบเทียบประสิทธิภาพกับการใช้ตัวแปรธรรมดาสำหรับค่าคงที่

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import {
-  type Component,
-  createEffect,
-  createMemo,
-  createSignal,
-} from 'solid-js'
+import { type Component, createMemo, createSignal } from 'solid-js'
 import { css, type Styles } from '#panda/css'
-
-const buttonStyle: Styles = {
-  bgColor: 'gray.200',
-  padding: '.5rem 1rem',
-  dropShadow: 'md',
-  borderRadius: '0.5rem',
-  cursor: 'pointer',
-  margin: '1rem 0',
-  _hover: {
-    bgColor: 'gray.300',
-  },
-}
 
 const heading1: Styles = {
   fontSize: '2.8rem',
@@ -26,31 +9,28 @@ const heading1: Styles = {
 }
 
 const App: Component = () => {
-  const [counter1, setCounter1] = createSignal(20)
-  const double1 = createMemo(() => {
-    console.log('double1')
-    return counter1() * 2
-  })
-
-  const double2 = () => {
-    console.log('double2')
-    return counter1() * 2
-  }
-
-  const [double3, _setCounter3] = createSignal(() => {
-    console.log('double3')
-    return counter1() * 2
-  })
-
-  const [double4, setDouble4] = createSignal(0)
-
-  createEffect(
-    () => counter1() * 2,
-    (value) => {
-      console.log('double4: ', value)
-      setDouble4(value)
+  const [counter, setCounter] = createSignal(
+    (console.log('createSignal first'), 20),
+    {
+      pureWrite: true,
     },
   )
+
+  const memo = createMemo(
+    normalFunction,
+    (console.log('init in createMemo'), 55),
+    {
+      equals(_prev, _curr) {
+        return false
+      },
+    },
+  )
+
+  function normalFunction(prev: number, current: number) {
+    console.log('normalFunction: ', prev, current)
+    counter()
+    return 2
+  }
 
   return (
     <div
@@ -58,29 +38,20 @@ const App: Component = () => {
         { bgColor: 'amber.400', border: '2px solid', borderColor: 'amber.800' },
         { paddingInline: '6rem', paddingBlock: '1rem', marginBlock: '2rem' },
       )}>
-      <header class={css(heading1)}>#1 createSignal ด้วย primitive value</header>
-      <p>{double1()}</p>
-      <p>{double1()}</p>
-      <br />
-
-      <p>{double2()}</p>
-      <p>{double2()}</p>
-      <br />
-
-      <p>{double3()}</p>
-      <p>{double3()}</p>
-      <br />
-
-      <p>{double4()}</p>
-      <p>{double4()}</p>
-      <br />
-
-      <h2 class={css(heading1)}>Counter: {counter1()}</h2>
+      <header class={css(heading1)}>
+        ทดลองการใช้งาน ตัวแปรธรรมดา (const val = ...):
+        ดีที่สุดสำหรับค่าคงที่ที่คำนวณครั้งเดียวจบ เพราะไม่มี Overhead ของระบบ Reactive
+      </header>
       <button
-        onClick={() => setCounter1(counter1() + 1)}
-        class={css(buttonStyle)}>
+        onClick={() => setCounter(counter() + 1)}
+        class={css({ bg: 'red.400', p: '1rem' })}>
         Increment
       </button>
+
+      <p>{(console.log('hello memo1'), memo())}</p>
+      <p>{(console.log('hello memo2'), memo())}</p>
+      <p>{(console.log('hello memo3'), memo())}</p>
+      {(console.log('++++++++++++++++++'), `  `)}
     </div>
   )
 }
